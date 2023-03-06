85675bf982db60c9278e331052c2c50f8d965164
BK201-Drama <1243532812@qq.com>
2023-03-07 00:24:03 +0800

    chore: add styles

---
+++ src/core/stores/index.js
@@ -0,0 +1,11 @@
+import { createContext, useContext } from 'react'
+
+function createStores() {
+  return {}
+}
+
+export const stores = createStores()
+
+export const StoresContext = createContext(stores)
+
+export const useStores = () => useContext(StoresContext)
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { ConfigProvider } from 'antd'
+import 'antd/dist/reset.css'
+import zhCN from 'antd/locale/zh_CN'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './index.css'
+import { stores, StoresContext } from './core/stores'
+import './styles/tailwind.less'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ConfigProvider locale={zhCN}>
+    <StoresContext.Provider value={stores}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StoresContext.Provider>
+  </ConfigProvider>
 )
