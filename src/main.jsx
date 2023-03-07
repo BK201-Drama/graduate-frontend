f28684e8682877091d65ee77e923dda35c004ffe
BK201-Drama <1243532812@qq.com>
2023-03-07 20:16:38 +0800

    feat: tailwind finish

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { ConfigProvider } from 'antd'
-import 'antd/dist/reset.css'
+// import 'antd/dist/reset.css'
 import zhCN from 'antd/locale/zh_CN'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { stores, StoresContext } from './core/stores'
-import './styles/tailwind.less';
+import './styles/tailwind.less'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ConfigProvider locale={zhCN}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
