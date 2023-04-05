9f0cd992a61dd63f204cb580fa6a2eba9af06738
BK201-Drama <1243532812@qq.com>
2023-04-05 17:55:18 +0800

    chore(env): use env to controll baseURL

--- src/core/services/axios.js
+++ src/core/services/axios.js
@@ -1,20 +1,20 @@
-import originAxios from 'axios';
-import { requestType } from '@/public/constants';
+import originAxios from 'axios'
+import { requestType } from '@/public/constants'
 
 // 创建请求实例
 const instance = originAxios.create({
-  baseURL: 'http://192.168.1.39:5000/api',
+  baseURL: `${import.meta.VITE_BASE_URL}/api`,
   timeout: 30000,
-});
+})
 
 const request = (url, params, type) => {
   let realData = {
     data: params,
-  };
+  }
   if (type === requestType.get) {
     realData = {
       params,
-    };
+    }
   }
 
   return new Promise((resolve, reject) => {
@@ -29,18 +29,17 @@ const request = (url, params, type) => {
     })
       .then((res) => resolve(res))
       .catch((err) => {
-        message.error('请求异常');
-        reject(err);
+        message.error('请求异常')
+        reject(err)
       })
       .finally(() => {
         // TODO: 添加加载spin
-      });
-  });
-};
+      })
+  })
+}
 
-export const get = (url, params) => request(url, params, requestType.get);
-export const post = (url, params) => request(url, params, requestType.post);
-export const patch = (url, params) => request(url, params, requestType.patch);
-export const deleted = (url, params) =>
-  request(url, params, requestType.delete);
-export const put = (url, params) => request(url, params, requestType.put);
+export const get = (url, params) => request(url, params, requestType.get)
+export const post = (url, params) => request(url, params, requestType.post)
+export const patch = (url, params) => request(url, params, requestType.patch)
+export const deleted = (url, params) => request(url, params, requestType.delete)
+export const put = (url, params) => request(url, params, requestType.put)
